a9a0635abf7ab82d0a9db258f63cf5b67babc18b
BGPKChaitanya <97452857+BGPKChaitanya@users.noreply.github.com>
2023-09-15 20:35:51 +0530

    push update
    push update

--- src/App.js
+++ src/App.js
@@ -3,13 +3,24 @@ import About from "./pages/about";
 import "./App.css";
 import "./index.css";
 import Home from "./pages/Home";
+import vanImage from "./assets/images/road-trip-van-cartoon-style-png.png";
 
 function App() {
   return (
     <BrowserRouter>
       <header>
         <Link className="site-logo" to="/">
-          #VanLife
+          <div
+            style={{
+              display: "flex",
+              flexDirection: "row",
+              justifyContent: "center",
+              alignItems: "center",
+            }}
+          >
+            <img src={vanImage} alt="aboutImage" className="vanLife-image" />{" "}
+            <p>VanLife</p>
+          </div>
         </Link>
         <nav>
           <Link to="/about">About</Link>
